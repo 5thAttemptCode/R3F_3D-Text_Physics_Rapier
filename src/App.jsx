c0c34314413ef06c92b49319506c2154fd86bbc6
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-08-10 18:11:56 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 
 import { Canvas } from '@react-three/fiber'
-import React from 'react'
+import React, { Suspense } from 'react'
 import Font from './Font'
-import { ContactShadows, OrbitControls } from '@react-three/drei'
+import { Box, ContactShadows, Environment, OrbitControls, Plane } from '@react-three/drei'
+import { Physics } from '@react-three/rapier'
+import { RigidBody } from '@react-three/rapier'
+
 
 export default function App( ) {
   return (
-    <Canvas camera={{position:[2, 2, 3], fov: 75}}>
-      <ContactShadows blur={0.9} opacity={0.2}/>
-      <mesh rotation-x={-Math.PI / 2} position-y={-0.1}>
-        <planeGeometry args={[5, 5]} />
-      </mesh>
+    <Canvas camera={{position:[1, 8, 4], fov: 55}}> 
+
       <ambientLight />
+      <directionalLight position={[-10, 10, 0]} />
+      <Environment preset="city" />
+
+      <spotLight intensity={1} angle={0.2} penumbra={1} position={[30, 30, 30]} castShadow shadow-mapSize={[512, 512]} />
       <OrbitControls />
-      <Font/>
+      <ContactShadows position-y={1} blur={0.9} opacity={1}/>
+
+      <Suspense>
+        <Physics>
+          <RigidBody type="fixed" restitution={1}>
+            <Plane receiveShadow position={[0, 0, 0]} args={[30, 30]} rotation-x={-Math.PI / 2}>
+              <meshStandardMaterial color="#592693" roughness={0.4} envMapIntensity={2} />
+            </Plane>
+          </RigidBody>
+          <Font />
+        </Physics>
+      </Suspense>
+
     </Canvas>
   )
 }
--- src/Font.jsx
+++ src/Font.jsx
@@ -1,14 +1,24 @@
 import { Center, Text3D } from '@react-three/drei'
+import { RigidBody } from '@react-three/rapier'
 
 export default function Font() {
   return (
-    <mesh position-y={0.5} rotation={[-0.5, 0, 0]}>
-        <Center>
-            <Text3D font="./Poppins_Bold.json">
-                HELLO
-                <meshStandardMaterial color="blue" />
-            </Text3D>
-        </Center>
-    </mesh>
+    <>
+      <RigidBody position={[-3, 4, 1]} rotation={[-2, 0, 0]}>
+        <Text3D font="./Pricedown_Regular.json"
+          curveSegments={ 1 }
+          bevelEnabled
+          bevelThickness={ 0.1 }
+          bevelSize={ 0.1 }
+          bevelOffset={ 0 }
+          bevelSegments={ 1 }
+          size={2}
+        >
+            WASTED
+            <meshStandardMaterial color="#141414" roughness={0} envMapIntensity={2} />
+        </Text3D>
+      </RigidBody>
+    </>
+
   )
 }
